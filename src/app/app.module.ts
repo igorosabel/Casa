6bfb90a49e0c7fb6ddc9e29a84855282e87d149f
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2022-11-20 12:48:17 +0100

    Actualizo Angular CLI (14.2.9 -> 15.0.0), Angular (14.2.10 -> 15.0.0), Angular Material (14.2.7 -> 15.0.0) y Angular CDK (14.2.7 -> 15.0.0)

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -1,47 +1,45 @@
-import { NgModule } from '@angular/core';
-import { BrowserModule } from '@angular/platform-browser';
 import { HttpClientModule, HTTP_INTERCEPTORS } from '@angular/common/http';
+import { NgModule } from '@angular/core';
 import { FormsModule } from '@angular/forms';
+import { BrowserModule } from '@angular/platform-browser';
 
 import { AppRoutingModule } from './app-routing.module';
-import { PAGES, COMPONENTS, PIPES, SERVICES, MATERIAL } from './app.common';
+import { COMPONENTS, MATERIAL, PAGES, PIPES, SERVICES } from './app.common';
 import { TokenInterceptor } from './interceptors/token.interceptor';
 
-import { AppComponent } from './app.component';
+import {
+  MatFormFieldDefaultOptions,
+  MAT_FORM_FIELD_DEFAULT_OPTIONS,
+} from '@angular/material/form-field';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
-import { MatFormFieldDefaultOptions, MAT_FORM_FIELD_DEFAULT_OPTIONS } from '@angular/material/form-field';
+import { AppComponent } from './app.component';
 
 const appearance: MatFormFieldDefaultOptions = {
-	appearance: 'outline'
+  appearance: 'outline',
 };
 
 @NgModule({
-	declarations: [
-		AppComponent,
-		...PAGES,
-		...COMPONENTS,
-		...PIPES
-	],
-	imports: [
-		BrowserModule,
-		HttpClientModule,
-		FormsModule,
-		AppRoutingModule,
-		BrowserAnimationsModule,
-		...MATERIAL
-	],
-	providers: [
-		{
-			provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
-			useValue: appearance
-		},
-		...SERVICES,
-		{
-			provide: HTTP_INTERCEPTORS,
-			useClass: TokenInterceptor,
-			multi: true
-		}
-	],
-	bootstrap: [AppComponent]
+  declarations: [AppComponent, ...PAGES, ...COMPONENTS, ...PIPES],
+  imports: [
+    BrowserModule,
+    HttpClientModule,
+    FormsModule,
+    AppRoutingModule,
+    BrowserAnimationsModule,
+    ...MATERIAL,
+  ],
+  providers: [
+    {
+      provide: MAT_FORM_FIELD_DEFAULT_OPTIONS,
+      useValue: appearance,
+    },
+    ...SERVICES,
+    {
+      provide: HTTP_INTERCEPTORS,
+      useClass: TokenInterceptor,
+      multi: true,
+    },
+  ],
+  bootstrap: [AppComponent],
 })
 export class AppModule {}
