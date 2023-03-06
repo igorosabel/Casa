ad48df2b4bfbb1bf742b7fe2e58b5945305183cf
Iñigo Gorosabel <inigo.gorosabel@gmail.com>
2023-03-06 09:44:09 +0100

    Refactorizaciones

--- src/app/interceptors/token.interceptor.ts
+++ src/app/interceptors/token.interceptor.ts
@@ -1,24 +1,29 @@
-import { Injectable } from '@angular/core';
 import {
-	HttpRequest,
-	HttpHandler,
-	HttpEvent,
-	HttpInterceptor
+  HttpEvent,
+  HttpHandler,
+  HttpInterceptor,
+  HttpRequest,
 } from '@angular/common/http';
-import { Utils } from '../services/utils.class';
-import { UserService } from '../services/user.service';
+import { Injectable } from '@angular/core';
 import { Observable } from 'rxjs';
+import { UserService } from 'src/app/services/user.service';
+import { Utils } from 'src/app/services/utils.class';
 
 @Injectable()
 export class TokenInterceptor implements HttpInterceptor {
-	constructor(public us: UserService) {}
+  constructor(public us: UserService) {}
 
-	intercept(request: HttpRequest<any>, next: HttpHandler): Observable<HttpEvent<any>> {
-		request = request.clone({
-			setHeaders: {
-				Authorization: this.us.user.token ? Utils.urldecode(this.us.user.token) : ''
-			}
-		});
-		return next.handle(request);
-	}
+  intercept(
+    request: HttpRequest<any>,
+    next: HttpHandler
+  ): Observable<HttpEvent<any>> {
+    request = request.clone({
+      setHeaders: {
+        Authorization: this.us.user.token
+          ? Utils.urldecode(this.us.user.token)
+          : '',
+      },
+    });
+    return next.handle(request);
+  }
 }
